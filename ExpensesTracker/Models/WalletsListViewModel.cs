7de059a69b2e230b09f8b1f755416c2af9a8e267
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2024-11-23 16:46:18 +0100

    removed old broken project

--- ExpensesTracker/Models/WalletsListViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using ExpensesTracker.Common.EntityModel.Sqlite;
-
-namespace ExpensesTracker.Models;
-
-public class WalletsListViewModel
-{
-    public List<WalletViewModel> Wallets { get; set; }
-}
