3e27aba427e0235011c765ebc86a04a430ac009c
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2024-12-20 01:53:55 +0100

    fixed cors issues

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyAllowedOrigins", policy =>
     {
-        policy.WithOrigins("https://localhost:7262", "https://localhost:5125")
+        policy.WithOrigins("https://localhost:7262")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
