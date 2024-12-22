84788c972729dd27ca14b0668f2e561f0142938e
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2024-12-22 21:12:44 +0100

    reorganize project structure

--- ExpensesTracker.Blazor/ExpensesTracker.Blazor.Client/IWalletController.cs
+++
@@ -1,8 +0,0 @@
-using ExpensesTracker.Models;
-
-namespace ExpensesTracker.Blazor.Client;
-
-public interface IWalletController
-{
-    public Task<WalletsListViewModel> Wallets();
-}
--- ExpensesTracker.Common.EntityModel.Sqlite/Label.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace ExpensesTracker.Common.EntityModel.Sqlite;
-
-public class Label : BaseDefinition
-{
-
-}
