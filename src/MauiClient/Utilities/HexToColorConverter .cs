b74509a74b737073030902c269baa58ef036e97f
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2024-12-27 22:43:33 +0100

    added color converter & fixed the category and label badges

---
+++ src/MauiClient/Utilities/HexToColorConverter .cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+
+namespace MauiClient.Utilities
+{
+    class HexToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if(value is string hex && !string.IsNullOrWhiteSpace(hex))
+            {
+                try
+                {
+                    return Color.FromArgb(hex);
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return Colors.Transparent;
+                }
+            }
+
+            return Colors.Transparent;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
